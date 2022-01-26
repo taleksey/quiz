3738ac4953f18e778c41dcc86c5b40b1be076375
Aleksey Terehov <alterehov@gmail.com>
2022-01-26 12:24:09 +0200

    Save customers in DB when customers login through auth0

---
+++ src/Presentation/Transformers/Auth0/ResponseToCustomerTransformer.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presentation\Transformers\Auth0;
+
+use App\Domain\Customer\Entity\Customer;
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+
+class ResponseToCustomerTransformer
+{
+    public function transform(UserResponseInterface $userResponse): Customer
+    {
+        $userRawData = $userResponse->getData();
+        $firstName = $userResponse->getFirstName() ?? $userRawData['given_name'] ?? '';
+        $lastName = $userResponse->getLastName() ?? $userRawData['family_name'] ?? '';
+        $isEmailVerified = $userRawData['email_verified'] ?? false;
+
+        $customer = new Customer();
+        $customer->setEmail($userResponse->getEmail());
+        $customer->setNickName($userResponse->getNickname());
+        $customer->setFirstName($firstName);
+        $customer->setLastName($lastName);
+        $customer->setIsVerified($isEmailVerified);
+        $customer->setPassword('');
+        return $customer;
+    }
+}
